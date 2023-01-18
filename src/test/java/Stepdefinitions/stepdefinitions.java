76de6d4cfc7ae9428d5fe95c0c130712765972c9
MTEJA <MTEJA@LAPTOP-KNCJI4TL>
2023-01-18 15:11:05 +0530

    this is the first commit

---
+++ src/test/java/Stepdefinitions/stepdefinitions.java
@@ -0,0 +1,23 @@
+package Stepdefinitions;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.When;
+
+public class stepdefinitions {
+	WebDriver driver ;
+	String url = "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login";
+	@Given("^I want to write a step with precondition$")
+	public void i_want_to_write_a_step_with_precondition() 
+	{
+		driver = new ChromeDriver();
+			driver.get(url);
+	}
+	@When("^opens browser$")
+	public void opens_browser() throws Throwable {
+	    
+	}
+
+}
---
+++ src/test/java/testrunner/testrunner.java
@@ -0,0 +1,12 @@
+package testrunner;
+
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features = "features/login.feature",glue = "Stepdefinitions",dryRun = false)
+public class testrunner {
+
+}
